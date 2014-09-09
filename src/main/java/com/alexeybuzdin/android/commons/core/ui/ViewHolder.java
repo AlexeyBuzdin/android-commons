68f88e2db563cd286a402917396d4d06fde0e659
Alexey Buzdin <alex.buzdin@gmail.com>
2014-09-09 18:48:06 +0300

    Added generic ViewHolder

---
+++ src/main/java/com/alexeybuzdin/android/commons/core/ui/ViewHolder.java
@@ -0,0 +1,32 @@
+package com.alexeybuzdin.android.commons.core.ui;
+
+import android.view.View;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ViewHolder {
+
+    private Map<Integer, View> holder = new HashMap<Integer, View>();
+
+    public <T extends View> T get(Integer id) {
+        return (T) holder.get(id);
+    }
+
+    public void put(View view) {
+        put(view, view.getId());
+    }
+
+    public void put(View view, Integer id) {
+        holder.put(id, view);
+    }
+
+    public void putViewFromParent(View parent, Integer id) {
+        View view = parent.findViewById(id);
+        put(view, id);
+    }
+
+    public void clear() {
+        holder.clear();
+    }
+}
